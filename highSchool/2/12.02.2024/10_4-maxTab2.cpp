8bcf8490b54dde9cd0ab8aa452c426d208aa67c1
Wolfyxon <wolfyxon@gmail.com>
2024-02-12 15:29:14 +0100

    Added task 10_4

---
+++ highSchool/2/12.02.2024/10_4-maxTab2.cpp
@@ -0,0 +1,45 @@
+// Napisz program znajdujący maksymalny element - tablicy dwuwymiarowej
+#ifndef SIGNATURE
+   #define SIGNATURE ""
+#endif
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main(int argc, char *argv[]) {
+    cout << SIGNATURE;
+    cout << "Maksymalny element tablicy 2 wymiarowej" << std::endl;
+
+    int rows;
+    int cols;
+
+    cout << "Podaj liczbe wierszy: ";
+    cin >> rows;
+
+    cout << "Podaj liczbe kolumn: ";
+    cin >> cols;
+
+    int array[rows][cols];
+
+    for(int row = 0; row < rows; row++) {
+        for(int col = 0; col < cols; col++) {
+            cout << "Podaj wartosc (" << row << ", " << col << "): ";
+            cin >> array[row][col];
+        }
+    }
+
+    int max = INT_MIN;
+    for(int row = 0; row < rows; row++) {
+        for(int col = 0; col < cols; col++) {
+            int num = array[row][col];
+            if(num > max) max = num;
+        }
+    }
+
+    cout << "Wynik: " << max << std::endl;
+
+    system("pause");
+    return 0;
+}
