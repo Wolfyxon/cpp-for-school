060609621c19d2c4f51ff60d17c641214fb0a617
Wolfyxon <wolfyxon@gmail.com>
2024-02-12 15:08:30 +0100

    Added task 2_4

---
+++ highSchool/2/12.02.2024/2_4-sumEvenTab2.cpp
@@ -0,0 +1,46 @@
+// Napisz program sumujący elementy parzyste - tablicy dwuwymiarowej
+#ifndef SIGNATURE
+   #define SIGNATURE ""
+#endif
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main(int argc, char *argv[]) {
+    cout << SIGNATURE;
+    cout << "Suma parzystych elementow tablicy 2 wymiarowej" << std::endl;
+
+    int rows;
+    int cols;
+
+    cout << "Podaj liczbe wierszy: ";
+    cin >> rows;
+
+    cout << "Podaj liczbe kolumn: ";
+    cin >> cols;
+
+    int array[rows][cols];
+
+    for(int row = 0; row < rows; row++) {
+        for(int col = 0; col < cols; col++) {
+            cout << "Podaj wartosc (" << row << ", " << col << "): ";
+            cin >> array[row][col];
+        }
+    }
+
+    int sum = 0;
+    for(int row = 0; row < rows; row++) {
+        for(int col = 0; col < cols; col++) {
+            int num = array[row][col];
+            if(num % 2 != 0) continue;
+            sum += num;
+        }
+    }
+
+    cout << "Wynik: " << sum << std::endl;
+
+    system("pause");
+    return 0;
+}
