6894920d84cdc8a674896580871fd59e8aa04271
Wolfyxon <wolfyxon@gmail.com>
2024-02-12 15:04:50 +0100

    Added task 1_4

---
+++ highSchool/2/12.02.2024/1_4-sumTab2.cpp
@@ -0,0 +1,45 @@
+// Napisz program sumujący elementy tablicy dwuwymiarowej
+
+#ifndef SIGNATURE
+   #define SIGNATURE ""
+#endif
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main(int argc, char *argv[]) {
+    cout << SIGNATURE;
+    cout << "Suma tablicy 2 wymiarowej" << std::endl;
+
+    int rows;
+    int cols;
+
+    cout << "Podaj liczbe wierszy: ";
+    cin >> rows;
+
+    cout << "Podaj liczbe kolumn: ";
+    cin >> cols;
+
+    int array[rows][cols];
+
+    for(int row = 0; row < rows; row++) {
+        for(int col = 0; col < cols; col++) {
+            cout << "Podaj wartosc (" << row << ", " << col << "): ";
+            cin >> array[row][col];
+        }
+    }
+
+    int sum = 0;
+    for(int row = 0; row < rows; row++) {
+        for(int col = 0; col < cols; col++) {
+            sum += array[row][col];
+        }
+    }
+
+    cout << "Wynik: " << sum << std::endl;
+
+    system("pause");
+    return 0;
+}
