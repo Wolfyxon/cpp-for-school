f03895716aa87e97836504ecea0ddaed97624e9d
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-02-17 17:16:10 +0100

    Fixed missing endl

--- highSchool/2/12.02.2024/20_4-maxRowTab2 copy.cpp
+++ highSchool/2/12.02.2024/20_4-maxRowTab2 copy.cpp
@@ -41,7 +41,7 @@ int main(int argc, char *argv[]) {
             int num = array[row][col];
             if(num > max) max = num;
         }
-        cout << row << ": " << max;
+        cout << row << ": " << max << std::endl;
     }
 
     system("pause");
