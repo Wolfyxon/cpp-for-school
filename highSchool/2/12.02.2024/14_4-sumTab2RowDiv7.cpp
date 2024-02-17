45c468960e55f0e279c1a0c09cd116e381382571
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-02-17 17:06:14 +0100

    Added task 14_4

---
+++ highSchool/2/12.02.2024/14_4-sumTab2RowDiv7.cpp
@@ -0,0 +1,48 @@
+// Napisz program sumujący elementy podzielne przez 7 w każdym wierszu
+
+#ifndef SIGNATURE
+   #define SIGNATURE ""
+#endif
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main(int argc, char *argv[]) {
+    cout << SIGNATURE;
+    cout << "Suma kazdego wiersza tablicy 2 wymiarowej" << std::endl;
+
+    int rows;
+    int cols;
+
+    cout << "Podaj liczbe wierszy: ";
+    cin >> rows;
+
+    cout << "Podaj liczbe kolumn: ";
+    cin >> cols;
+
+    int array[rows][cols];
+
+    for(int row = 0; row < rows; row++) {
+        for(int col = 0; col < cols; col++) {
+            cout << "Podaj wartosc (" << row << ", " << col << "): ";
+            cin >> array[row][col];
+        }
+    }
+
+
+    cout << "-- Wyniki: --" << std::endl;
+
+    for(int row = 0; row < rows; row++) {
+        int sum = 0;
+        for(int col = 0; col < cols; col++) {
+            int num = array[row][col];
+            if(num % 7 == 0) sum += num;
+        }
+        cout << row << ": " << sum << std::endl;
+    }
+
+    system("pause");
+    return 0;
+}
