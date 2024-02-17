299cddae950e5d29972ab802eeb0bf451d56d32f
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-02-17 16:55:41 +0100

    Added task 12_4

---
+++ highSchool/2/12.02.2024/12_4-sumTab2Row.cpp
@@ -0,0 +1,47 @@
+// Napisz program sumujący elementy w każdym wierszu - tablicy dwuwymiarowej
+
+#ifndef SIGNATURE
+   #define SIGNATURE ""
+#endif
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main(int argc, char *argv[]) {
+    cout << SIGNATURE;
+    cout << "Suma kazdego wiersza tablicy 2 wymiarowej" << std::endl;
+
+    int rows;
+    int cols;
+
+    cout << "Podaj liczbe wierszy: ";
+    cin >> rows;
+
+    cout << "Podaj liczbe kolumn: ";
+    cin >> cols;
+
+    int array[rows][cols];
+
+    for(int row = 0; row < rows; row++) {
+        for(int col = 0; col < cols; col++) {
+            cout << "Podaj wartosc (" << row << ", " << col << "): ";
+            cin >> array[row][col];
+        }
+    }
+
+
+    cout << "-- Wyniki: --" << std::endl;
+
+    for(int row = 0; row < rows; row++) {
+        int sum = 0;
+        for(int col = 0; col < cols; col++) {
+            sum += array[row][col];
+        }
+        cout << row << ": " << sum << std::endl;
+    }
+
+    system("pause");
+    return 0;
+}
