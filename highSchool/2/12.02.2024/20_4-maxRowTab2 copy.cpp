5649259275032e2a75df6e133185b8bc55819ed2
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-02-17 17:09:45 +0100

    Added task 20_4

---
+++ highSchool/2/12.02.2024/20_4-maxRowTab2 copy.cpp
@@ -0,0 +1,49 @@
+// Napisz program znajdujący maksymalny element w każdym wierszu
+
+#ifndef SIGNATURE
+   #define SIGNATURE ""
+#endif
+
+#include <iostream>
+#include <limits.h>
+
+using std::cout;
+using std::cin;
+
+int main(int argc, char *argv[]) {
+    cout << SIGNATURE;
+    cout << "Najwieksze elementy wierszy tablicy 2 wymiarowej" << std::endl;
+
+    int rows;
+    int cols;
+
+    cout << "Podaj liczbe wierszy: ";
+    cin >> rows;
+
+    cout << "Podaj liczbe kolumn: ";
+    cin >> cols;
+
+    int array[rows][cols];
+
+    for(int row = 0; row < rows; row++) {
+        for(int col = 0; col < cols; col++) {
+            cout << "Podaj wartosc (" << row << ", " << col << "): ";
+            cin >> array[row][col];
+        }
+    }
+
+
+    cout << "-- Najwieksze liczby: --" << std::endl;
+
+    for(int row = 0; row < rows; row++) {
+        int max = INT_MIN;
+        for(int col = 0; col < cols; col++) {
+            int num = array[row][col];
+            if(num > max) max = num;
+        }
+        cout << row << ": " << max;
+    }
+
+    system("pause");
+    return 0;
+}
