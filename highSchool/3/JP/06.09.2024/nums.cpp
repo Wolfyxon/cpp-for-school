26f4d2e8a9bcc0cb7168266a5c85ae3090d2b3da
Wolfyxon <wolfyxon@gmail.com>
2024-09-06 15:28:30 +0200

    Init 3rd grade

---
+++ highSchool/3/JP/06.09.2024/nums.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+enum Operation {
+    ADD,
+    SUB,
+    MUL,
+    DIV
+};
+
+const int OperationLen = 4;
+
+void printOperation(int i, char* name) {
+    cout << i << ") " << name << endl;
+}
+
+int getOperation() {
+    printOperation(0, "Dodawanie");
+    printOperation(1, "Odejmowanie");
+    printOperation(2, "Mnożenie");
+    printOperation(3, "Dzielenie");
+    
+    int oper;
+
+    cout << "Wybierz operację: ";
+    cin >> oper;
+
+    if(oper > OperationLen - 1 || oper < 0) {
+        cout << "Nie znana operacja. Spróbuj ponownie." << endl;
+        return getOperation();
+    }
+
+    return oper;
+}
+
+int main(int argc, char* argv[]) {
+    int amt;
+    int oper = getOperation();
+    int res = 0;
+
+    cout << "Podaj ilość: ";
+    cin >> amt;
+    
+    for(int i = 0; i < amt; i++) {
+        int n;
+
+        cout << "Podaj liczbę nr. " << i + 1 << ": ";
+        cin >> n;
+
+        switch (oper) {
+        case Operation::ADD:
+            res += n;
+            break;
+        
+        case Operation::SUB:
+            res -= n;
+            break;
+        
+        case Operation::MUL:
+            res *= n;
+            break;
+        
+        case Operation::DIV:
+            res /= n;
+            break;
+        }
+    }
+
+    cout << "Wynik wynosi: " << res;
+
+    return 0;
+}
