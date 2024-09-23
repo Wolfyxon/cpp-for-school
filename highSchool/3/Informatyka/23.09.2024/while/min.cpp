07d6717e9dbcb9ced47f964d99e4790ba22bd21f
Wolfyxon <wolfyxon@gmail.com>
2024-09-23 14:52:43 +0200

    Added while/min.cpp

---
+++ highSchool/3/Informatyka/23.09.2024/while/min.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <limits>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(int argc, char* argv[]) {
+    
+    int min = std::numeric_limits<int>::max();
+    int i = 0;
+    int current;
+
+    cout << "(Wpisanie 0 zatrzyma program)" << endl;
+
+    while(true) {
+        i++;
+
+        cout << "Podaj" << i << " liczbę: ";
+        cin >> current;
+
+        if(current == 0) break;
+
+        if(current < min) {
+            min = current;
+        }
+    }
+
+    cout << endl << "Najmniejsza wartość: " << min << endl;
+    
+    return 0;
+}
