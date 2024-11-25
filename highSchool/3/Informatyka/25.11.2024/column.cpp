853025a0e9810557981b164c34dccc317e9610c5
= <wolfyxon@gmail.com>
2024-11-25 13:18:51 +0100

    Implemented the encryption

--- highSchool/3/Informatyka/25.11.2024/column.cpp
+++ highSchool/3/Informatyka/25.11.2024/column.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
+#include <string>
 
+using std::string;
 using std::cout;
 using std::cin;
 using std::endl;
 
 int main(void) {
-    int key;
+    string text;
+    cout << "Podaj jawny tekst: ";
+    cin >> text;
 
+    int key;
     cout << "Podaj klucz: ";
     cin >> key;
+
+    string encrypted;
+
+    for(int i = 0; i < key; i++) {
+        for(int ii = 0; ii < encrypted.length(); ii++) {
+            encrypted += text[ii];
+        }
+    }
+
+    cout << "Zaszyfrowany text:" << endl;
+    cout << encrypted;
 }
