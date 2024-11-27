be6d74f14f8293250eb40bfd8fcf61c832d07052
= <wolfyxon@gmail.com>
2024-11-27 07:31:20 +0100

    Querying

--- highSchool/3/JezykiProgramowania/27.11.2024/toBin.cpp
+++ highSchool/3/JezykiProgramowania/27.11.2024/toBin.cpp
@@ -15,6 +15,27 @@ const string SYSTEM_NAMES[] = {
     "Binarny"
 };
 
+System inputSystem(string query) {
+    int inp;
+
+    cout << query << ": ";
+    cin >> inp;
+
+    if(inp < 1 || inp > sizeof(SYSTEM_NAMES) / sizeof(string)) {
+        cout << "Nieznany system" << endl;
+        return inputSystem(query);
+    }
+
+    return (System) (inp - 1);
+}
+
 int main(void) {
+    for(int i = 0; i < sizeof(SYSTEM_NAMES) / sizeof(string); i++) {
+        cout << i + 1 << ". " << SYSTEM_NAMES[i] << endl;;
+    }
+
+    System from = inputSystem("Podaj źródłowy system");
+    System to = inputSystem("Podaj końcowy system");
+
     return 0;
 }
