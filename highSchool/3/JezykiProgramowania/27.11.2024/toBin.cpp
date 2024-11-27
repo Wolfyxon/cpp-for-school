d74e72237e03fac43db1b57cbf24ed220ddcafb8
= <wolfyxon@gmail.com>
2024-11-27 07:24:49 +0100

    Init 27.11

---
+++ highSchool/3/JezykiProgramowania/27.11.2024/toBin.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+enum System {
+    DECIMAL,
+    BINARY
+};
+
+const string SYSTEM_NAMES[] = {
+    "Dziesietny"
+};
+
+int main(void) {
+    return 0;
+}
