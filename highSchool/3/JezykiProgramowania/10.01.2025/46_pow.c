b209ea791db7f62cd88dda556e4b39a80fb39525
= <wolfyxon@gmail.com>
2025-01-10 10:51:17 +0100

    init 46

---
+++ highSchool/3/JezykiProgramowania/10.01.2025/46_pow.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int x, y;
+
+    printf("Podaj liczbę i potęgę (oddzielone spacjami): ");
+    scanf("%i %i", &x, &y);
+
+    int n = x;
+    
+    for(int i = 0; i < y; i++) {
+        n *= x;
+    }
+}
