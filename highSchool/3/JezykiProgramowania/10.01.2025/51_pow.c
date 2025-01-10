6493f1ba99647f9e7c0a17ad116e58e17f8c038d
Wolfyxon <wolfyxon@gmail.com>
2025-01-10 16:42:13 +0100

    Using recursion

--- highSchool/3/JezykiProgramowania/10.01.2025/51_pow.c
+++ highSchool/3/JezykiProgramowania/10.01.2025/51_pow.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
+int pow(int x, int y) {
+    if(y == 1) return x;
+
+    return pow(x * x, y - 1);
+} 
+
 int main() {
     int x, y;
 
     printf("Podaj liczbę i potęgę (oddzielone spacjami): ");
     scanf("%i %i", &x, &y);
 
-
+    printf("Wynik: %i \n", pow(x, y));
 }
