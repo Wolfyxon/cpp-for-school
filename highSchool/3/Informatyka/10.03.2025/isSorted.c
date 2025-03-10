81a23fc2c138ca37758dbf3a65962c5e50c31b87
= <wolfyxon@gmail.com>
2025-03-10 13:17:30 +0100

    Added isSorted

---
+++ highSchool/3/Informatyka/10.03.2025/isSorted.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    int len = 0;
+    int* nums = malloc(0);
+
+    printf("Podawaj liczby, lub x by zakończyć: \n");
+
+    while(1) {
+        char buf[16];
+        scanf("%s", buf);
+
+        if(strcmp(buf, "x") == 0) {
+            break;
+        }
+
+        len++;
+        nums = realloc(nums, len * sizeof(int));
+
+        if(nums == NULL) {
+            puts("RAM sie pali");
+            return 1;
+        }
+
+        nums[len - 1] = atoi(buf);
+    }
+
+    for(int i = 0; i < len - 1; i++) {
+        if(nums[i] > nums[i + 1]) {
+            puts("Lista nie posortowana");
+            return;
+        }
+    }
+
+    puts("Lista posortowana");
+}
