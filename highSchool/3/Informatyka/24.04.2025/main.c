21d68004aed97f103139cc4e43f1b1505d2f0411
= <wolfyxon@gmail.com>
2025-03-24 13:29:22 +0100

    Added 24.04.2025

---
+++ highSchool/3/Informatyka/24.04.2025/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+const float NOMINALS[] = {20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.01};
+const int NOMINALS_LEN = sizeof(NOMINALS) / sizeof(float);
+
+int main() {
+    float change;
+
+    printf("Podaj resztę: ");
+    scanf("%f", &change);
+
+    int i = 0;
+
+    while(change > 0 && i < NOMINALS_LEN) {
+        float currentNominal = NOMINALS[i];
+
+        if(change >= currentNominal) {
+            printf("%f ", currentNominal);
+            change -= currentNominal;
+            continue;
+        }
+
+        i++;
+    }
+
+    puts("");
+}
