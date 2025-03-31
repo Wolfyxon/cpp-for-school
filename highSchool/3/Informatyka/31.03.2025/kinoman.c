b127654998c00b4531b032949a55c46f973db625
= <wolfyxon@gmail.com>
2025-03-31 13:28:51 +0200

    init 31.03.2025

---
+++ highSchool/3/Informatyka/31.03.2025/kinoman.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+typedef struct {
+    int start;
+    int end;
+} Film;
+
+void printFilm(Film* film, int id) {
+    printf("F%i: %i - %i \n", id, film->start, film->end);
+}
+
+void sort(Film* films, size_t len) {
+    
+    for(int i = 1; i < len; i++) {
+        int j = i - 1;
+        Film current = films[i];
+
+        while(j >= 0 && films[j].end > current.end) {
+            films[j + 1] = films[j];
+            j -= 1;
+        }
+
+        films[j + 1] = current;
+    }
+}
+
+int main() {
+    Film films[] = {
+        {
+            .start = 9,
+            .end = 10
+        },
+        {
+            .start = 16,
+            .end = 19
+        }
+    };
+
+    size_t filmsLen = sizeof(films) / sizeof(Film);
+
+    puts("Harmonogram:");
+
+    for(int i = 0; i < filmsLen; i++) {
+        printFilm(&films[i], i);
+    }
+
+    puts("Posortowane:");
+    sort(films, filmsLen);
+
+    for(int i = 0; i < filmsLen; i++) {
+        printFilm(&films[i], i);
+    }
+
+    
+}
