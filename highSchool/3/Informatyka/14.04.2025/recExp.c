da27c3e77ab276b2fc9d2b361314d044717e3280
= <wolfyxon@gmail.com>
2025-04-14 13:37:21 +0200

    oops

--- highSchool/3/Informatyka/14.04.2025/recExp.c
+++ highSchool/3/Informatyka/14.04.2025/recExp.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
 int getExpRec(int n) {
-    if(n == 0) return 1;
+    if(n == 1) return 1;
 
-    return 3 * getLayersRec(n - 1);
+    return 3 * getExpRec(n - 1);
 }
 
-int getExpIter(int folds) {
+int getExpIter(int n) {
     int res = 1;
 
-    for(int i = 0; i < folds; i++) {
-        res = 3 * res - 1;
+    for(int i = 1; i < n; i++) {
+        res = 3 * res;
     }
+    
+    return res;
 }
 
 int main() {
